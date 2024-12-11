3fa5a9c9e124605b2c0225ae80461704a0dba10e
cryptic-bazinga <ahickox@jds.org>
2024-12-11 17:43:04 +0000

    i just lost my dawg

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+import random
+jschlatt = 0
+for x in range(0,100):
+    jschlatt = jschlatt =random.randint(1,100)
+    print(jschlatt)
+    if jschlatt > 50:
+        print("i just lost my dawg")
+    else:
+        print("nope still have my dawg")
 
