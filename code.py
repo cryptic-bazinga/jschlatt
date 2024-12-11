475ff0a90f7ee25e4e8390e5a1d81a12b9c06d14
cryptic-bazinga <ahickox@jds.org>
2024-12-11 18:41:09 +0000

    rawr

--- code.py
+++ code.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import random
+import config
 jschlatt = 0
-for x in range(0,100):
+    for x in range(0,100):
     jschlatt = jschlatt =random.randint(1,100)
     print(jschlatt)
     if jschlatt > 50:
         print("i just lost my dawg")
+    elif jschlatt == 69:
+        print("freaky")
+        break
+    elif jschlatt == 42:
+        print("freaky")
     else:
         print("nope still have my dawg")
-
