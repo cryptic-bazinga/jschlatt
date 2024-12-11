bb4e6ddb3ed3ef5d0363615339fb0ed609fecb21
cryptic-bazinga <ahickox@jds.org>
2024-12-11 21:30:03 +0000

    gave config funcinailty

--- code.py
+++ code.py
@@ -2,15 +2,18 @@
 import random
 import config
 jschlatt = 0
-    for x in range(0,100):
+for x in range(0,100):
     jschlatt = jschlatt =random.randint(1,100)
     print(jschlatt)
     if jschlatt > 50:
         print("i just lost my dawg")
     elif jschlatt == 69:
         print("freaky")
-        break
+        if config.freaky == 1:
+            break
     elif jschlatt == 42:
         print("freaky")
+        if config.freaky == 1:
+            break
     else:
         print("nope still have my dawg")
