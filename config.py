76034697b7598fe369f3ee04687ac2fa11ce02c1
cryptic-bazinga <ahickox@jds.org>
2024-12-12 19:27:46 +0000

    fixed config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 freaky = 0 # set to one if you want to break on freaky or evyater
 evyater = 1 # set to one if you want to replace "freaky" with "I LOVE EVYATER!!!"
-fish = 1
+fish = 1 # repalces "nope still have my dawg" with "im a master baiter"
