5ea1080a2c2d2ab1ccbfdb7a9bc46a5699c67c5d
cryptic-bazinga <ahickox@jds.org>
2024-12-12 19:51:34 +0000

    code changes

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 import random
 import config
 jschlatt = 0
-for x in range(0,100):
+for x in range(0,config.loops):
     jschlatt = jschlatt =random.randint(1,100)
     print(jschlatt)
     if jschlatt > 50:
@@ -21,8 +21,10 @@ for x in range(0,100):
             print("freaky")
         if config.freaky == 1:
             break
+    elif jschlatt == 32:
+        print("Skibidi bop bop bop yes yes skibidi bododop beep beep idk")
     else:
         if config.fish == 1:
-            print("IM A MASTER BAITER")
+            print("IM A MASTER FISHER")
         else:
             print("nope still have my dawg")
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+loops = 100 # set to any number to be the amount of times the loop loops
 freaky = 0 # set to one if you want to break on freaky or evyater
-evyater = 1 # set to one if you want to replace "freaky" with "I LOVE EVYATER!!!"
-fish = 1 # repalces "nope still have my dawg" with "im a master baiter"
+evyater = 0 # set to one if you want to replace "freaky" with "I LOVE EVYATER!!!"
+fish = 0 # repalces "nope still have my dawg" with "im a master fisher"
