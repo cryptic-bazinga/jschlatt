d59f3e6a8cf8e54e9e8c375440df81b7d956ebad
cryptic-bazinga <ahickox@jds.org>
2025-01-30 20:15:39 +0000

    erm what the sigma

--- code.py
+++ code.py
@@ -13,7 +13,10 @@ for x in range(0,config.loops):
         else:
             print("freaky")
         if config.freaky == 1:
-            break
+            print("AHHH HELP")
+    elif jschlatt == 57:
+        print("ADAM IS A HOMOSEXUAL CONFIRMED")
+        break
     elif jschlatt == 42:
         if config.evyater == 1:
             print("I LOVE EVYATER!!!")
@@ -27,7 +30,7 @@ for x in range(0,config.loops):
         print("it is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife")
     elif jschlatt == 1:
         print("im bouta bust")
-        break
+        
     else:
         if config.fish == 1:
             print("IM A MASTER FISHER")
---
+++ gay.py
@@ -0,0 +1,4 @@
+import random
+for x in range(1, 100):
+    gay = gay =random.randint(1,100)
+    print(gay)
