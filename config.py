e7c0515c05cf2c4e4fc6835404d7215332c078fc
cryptic-bazinga <ahickox@jds.org>
2025-02-13 22:16:13 +0000

    removed whitespace

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-loops = 10000000000000000000000000000000000000000000000000000000 # set to any number to be the amount of times the loop loops
+loops = 100 # set to any number to be the amount of times the loop loops
 freaky = 0 # set to one if you want to break on freaky or evyater
 evyater = 0 # set to one if you want to replace "freaky" with "I LOVE EVYATER!!!"
 fish = 0 # replaces "nope still have my dawg" with "im a master fisher"
-final_number_true = 0 # prints final number
